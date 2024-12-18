763181d13f7e407ee72df466c25970d094e07582
Gabriel <gabrielbassabarreto@gmail.com>
2024-12-18 19:18:45 -0300

    atualização de telas

--- src/app/transaction-form/transaction-form.component.ts
+++ src/app/transaction-form/transaction-form.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { TransactionService } from '../transaction.service';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-transaction-form',
   standalone: true,
-  imports: [ FormsModule ],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],  
   templateUrl: './transaction-form.component.html',
   styleUrl: './transaction-form.component.scss'
 })
 export class TransactionFormComponent {
+  categories: any[] = [
+    {value: 'food', viewValue: 'Food'},
+    {value: 'transportation', viewValue: 'Transportation'},
+    {value: 'entertainment', viewValue: 'Entertainment'},
+  ];
   description = '';
   amount: number | null = null;
   type: 'income' | 'expense' = 'income';
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TransactionService } from '../transaction.service';
 import { TransactionFormComponent } from '../transaction-form/transaction-form.component';
 import { ChartComponent } from '../chart/chart.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [ TransactionFormComponent, ChartComponent ],
+  imports: [ TransactionFormComponent, ChartComponent, NgFor ],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.scss'
 })
